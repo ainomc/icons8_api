55efba470a449068b7cb7e0ef72a79ff2c679a33
Kusch-Artem <kuschartem2094@gmail.com>
2016-11-20 20:27:11 +0200

    add search tests

--- api_context.py
+++ api_context.py
@@ -1,17 +1,173 @@
 # -*- coding: utf-8 -*-
-from api_logic import *
+
 import paramiko
+
+from api_logic import random_between_values, random_list_value, request, all_tag_attrib, word_count, attrib_value
+
 # Fixture settings fo Tests class
-class Context(object):
+class ContextIconApi(object):
+
+
+    # Settings
+    # icon id what will be in request
+    icon_id = str(random_between_values(100, 20000))
+    print (icon_id + ' test icon')
+    payload = {'id': icon_id}
+
+    # Do Request and return response root
+    response_root = request('icon', payload)
 
     # Action before class
     def setup_class(cls):
         print("\n>>> Class Setup")
 
-        host = 'https://demoapi.icons8.com/'
-        user = 'ainomc@gmail.com'
-        secret = '123'
-        port = 22
+    #  Action after class
+    def teardown_class(cls):
+        print("\n>>> Class Teardown")
+
+class ContextSearchDefaultApi(object):
+
+    #settings
+    api_type = 'search'
+    term = random_list_value(
+        ['google', 'facebook', 'space',
+         'ball', 'car', 'word']
+    )
+    payload = {
+        'term': term, 'amount': '',
+        'offset': '', 'platform': ''
+    }
+    icon_count = 25
+
+    # Do Request and return response root
+    response_root = request(api_type, payload)
+
+    icons_current_count = 0
+    x = True
+    while x == True:
+        try:
+            icons_current_count += 1
+            tag_attribs = all_tag_attrib(response_root,
+                                         'icon', str(icons_current_count))
+            value_of_attrib = attrib_value(tag_attribs, 'id')
+            assert word_count(value_of_attrib) >= 1
+            assert icons_current_count <= icon_count
+        except AttributeError:
+            x = False
+            icons_current_count -= 1
+            print (str(icons_current_count) + '<<< count icons in response')
+            assert icons_current_count > 0
+    print (str(icons_current_count) + ' <<< current count of icons')
+
+    # Choose random icon between min and max
+    icon_number = str(random_between_values(1, icon_count))
+
+    # Action before class
+    def setup_class(cls):
+        print("\n>>> Class Setup")
+
+    #  Action after class
+    def teardown_class(cls):
+        print("\n>>> Class Teardown")
+
+class ContextSearchMaxApi(object):
+
+
+    #settings
+    api_type = 'search'
+    search_text = random_list_value(['google', 'facebook', 'space',
+                                     'ball', 'car', 'word'])
+    search_amount = '20'
+    all_platforms = ["win8", "ios7", "android", "androidL",
+                     "color", "win10", "office"]
+    search_platform = random_list_value(all_platforms)
+    search_offset = '20'
+    search_language = ''
+    payload = {
+        'term': search_text, 'amount': search_amount,
+        'offset': search_offset, 'platform': search_platform
+    }
+
+    icon_count = 20
+
+    # Do Request and return response root
+    response_root = request(api_type, payload)
+
+
+    icons_current_count = 0
+    x = True
+    while x == True:
+        try:
+            icons_current_count += 1
+            tag_attribs = all_tag_attrib(response_root,
+                                         'icon', str(icons_current_count))
+            value_of_attrib = attrib_value(tag_attribs, 'id')
+            assert word_count(value_of_attrib) >= 1
+            assert icons_current_count <= icon_count
+        except AttributeError:
+            x = False
+            icons_current_count -= 1
+            print (str(icons_current_count) + '<<< count icons in response')
+            assert icons_current_count > 0
+    print (str(icons_current_count) + ' <<< current count of icons')
+
+    # Choose random icon between min and max
+    icon_number = str(random_between_values(1, icons_current_count))
+
+    # Action before class
+    def setup_class(cls):
+        print("\n>>> Class Setup")
+
+    #  Action after class
+    def teardown_class(cls):
+        print("\n>>> Class Teardown")
+
+
+class ContextSearchMinApi(object):
+
+    #settings
+    api_type = 'search'
+    search_text = random_list_value(['google', 'facebook', 'space',
+                                     'ball', 'car', 'word'])
+    search_amount = '5'
+    all_platforms = ["win8", "ios7", "android", "androidL",
+                     "color", "win10", "office"]
+    search_platform = random_list_value(all_platforms)
+    search_offset = '5'
+    search_language = ''
+    payload = {
+        'term': search_text, 'amount': search_amount,
+        'offset': search_offset, 'platform': search_platform
+    }
+
+    icon_count = 5
+
+    # Do Request and return response root
+    response_root = request(api_type, payload)
+
+    icons_current_count = 0
+    x = True
+    while x == True:
+        try:
+            icons_current_count += 1
+            tag_attribs = all_tag_attrib(response_root,
+                                         'icon', str(icons_current_count))
+            value_of_attrib = attrib_value(tag_attribs, 'id')
+            assert word_count(value_of_attrib) >= 1
+            assert icons_current_count <= icon_count
+        except AttributeError:
+            x = False
+            icons_current_count -= 1
+            print (str(icons_current_count) + '<<< count icons in response')
+            assert icons_current_count > 0
+    print (str(icons_current_count) + ' <<< current count of icons')
+
+    # Choose random icon between min and max
+    icon_number = str(random_between_values(1, icon_count))
+
+    # Action before class
+    def setup_class(cls):
+        print("\n>>> Class Setup")
 
     #  Action after class
     def teardown_class(cls):
--- api_logic.py
+++ api_logic.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
-import requests
+
 import random
+
+import requests
 import xml.etree.ElementTree as ET
 
 
 
 # Do request, check response and return root of response. apiType - type of api, payload - values for url to xml
 def request(apiType, payload):
-    response = requests.get('https://demoapi.icons8.com/api/iconsets/%s' % apiType, params=payload)
+    response = requests.get('https://api.icons8.com/api/iconsets/%s' % apiType, params=payload)
     response.raise_for_status()
     return ET.fromstring(response.content)
+
 
 # Find xml key by xpath and return key value.
 def tag_value(root, tag, tag_xpath_number):
@@ -18,6 +21,14 @@ def tag_value(root, tag, tag_xpath_number):
 # Return all attributes from tag
 def all_tag_attrib(root, tag, tag_xpath_number):
     return root.find('.//%s[%s]' % (tag, tag_xpath_number)).attrib
+
+# Find xml key by xpath and return key value.
+def tag_value_with_icon_number(root, icon, tag, tag_xpath_number):
+    return root.find('.//icon[%s]/%s[%s]' % (icon, tag, tag_xpath_number)).text
+
+# Return all attributes from tag
+def all_tag_attrib_with_icon_number(root, icon, tag, tag_xpath_number):
+    return root.find('.//icon[%s]/%s[%s]' % (icon, tag, tag_xpath_number)).attrib
 
 # Return  attribute value
 def attrib_value(tag_all_attrib, Attrib):
@@ -28,12 +39,18 @@ def attrib_value(tag_all_attrib, Attrib):
 def random_between_values(first_value, last_value):
     return random.randint(first_value, last_value)
 
+# Возвращает случайное значение из списка
+def random_list_value(list):
+    return random.choice(list)
+
+
 def word_count(word):
     return len(word)
 
+
 def check_all_categories(test_categories):
-    all_categories = ["Windows 8/Metro", "iPhone/iOS 7", "Android", "Android L", "Color", "Windows 10/Threshold",
-                      "Office"]
+    all_categories = ["Windows 8/Metro", "iPhone/iOS 7", "Android", "Android L",
+                      "Color", "Windows 10/Threshold", "Office"]
     all_categories_count = len(all_categories)
     current_categories = 0
     for categories in all_categories:
