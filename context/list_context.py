e2af7b003ad36d1f8925972b3a56482583edede6
Kusch-Artem <kuschartem2094@gmail.com>
2016-12-21 23:59:23 +0200

    add part of list tests

---
+++ context/list_context.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import paramiko
+
+from api_logic import random_between_values, random_list_value, request, all_tag_attrib, word_count, attrib_value
+
+
+class ContextListApi(object):
+
+
+    # settings
+    api_type = 'list'
+
+    #search_platform = random_list_value(["win8", "ios7", "android", "androidL",
+    #                                     "color", "win10", "office", ""])
+    search_platform = random_list_value([""])
+
+    auth_id = '07cb0f621e742888b888d7630c1f0b37bdae536b'
+
+    payload = {'platform': search_platform, 'auth-id': auth_id}
+
+    response_root = request(api_type, payload)
+
+    # Action before class
+    def setup_class(cls):
+        print("\n>>> Class Setup")
+
+    #  Action after class
+    def teardown_class(cls):
+        print("\n>>> Class Teardown")
---
+++ tests/tests_list_api.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import paramiko
+
+from icons8_api.api_logic import attrib_value, tag_value, all_tag_attrib, word_count
+from context.list_context import ContextListApi
+
+
+"""
+python -m pytest -v tests_list_api.py -s     --   runner
+
+"""
+
+
+# Test Search Api with Default values. https://demoapi.icons8.com/manual/suggest
+class TestListApi(ContextListApi):
+
+
+    # Test 'list' 1 tag with any input
+    def test_list1_tag(self):
+
+        print(ContextListApi.search_platform + ' - platform')
+
+        if ContextListApi.search_platform == "":
+            tag_attribs = all_tag_attrib(ContextListApi.response_root,
+                                         'list/list', '1')
+            value_of_attrib = attrib_value(tag_attribs, 'platform')
+            assert value_of_attrib == "Windows 8/Metro"
+
+            value_of_attrib = attrib_value(tag_attribs, 'format')
+            assert value_of_attrib == "json"
+
+            value_of_attrib = attrib_value(tag_attribs, 'link')
+            assert value_of_attrib[-5:] == ".json"
+
+    # Test 'list' 2 tag with any input
+    def test_list2_tag(self):
+
+        if ContextListApi.search_platform == "":
+            tag_attribs = all_tag_attrib(ContextListApi.response_root,
+                                         'list/list', '2')
+            value_of_attrib = attrib_value(tag_attribs, 'platform')
+            assert value_of_attrib == "iPhone/iOS 7"
+
+            value_of_attrib = attrib_value(tag_attribs, 'format')
+            assert value_of_attrib == "json"
+
+            value_of_attrib = attrib_value(tag_attribs, 'link')
+            assert value_of_attrib[-5:] == ".json"
+
+    # Test 'list' 3 tag with any input
+    def test_list3_tag(self):
+
+        if ContextListApi.search_platform == "":
+            tag_attribs = all_tag_attrib(ContextListApi.response_root,
+                                         'list/list', '3')
+            value_of_attrib = attrib_value(tag_attribs, 'platform')
+            assert value_of_attrib == "Android"
+
+            value_of_attrib = attrib_value(tag_attribs, 'format')
+            assert value_of_attrib == "json"
+
+            value_of_attrib = attrib_value(tag_attribs, 'link')
+            assert value_of_attrib[-5:] == ".json"
+
+    # Test 'list' 4 tag with any input
+    def test_list4_tag(self):
+
+        if ContextListApi.search_platform == "":
+            tag_attribs = all_tag_attrib(ContextListApi.response_root,
+                                         'list/list', '4')
+            value_of_attrib = attrib_value(tag_attribs, 'platform')
+            assert value_of_attrib == "Android L"
+
+            value_of_attrib = attrib_value(tag_attribs, 'format')
+            assert value_of_attrib == "json"
+
+            value_of_attrib = attrib_value(tag_attribs, 'link')
+            assert value_of_attrib[-5:] == ".json"
+
+    # Test 'list' 5 tag with any input
+    def test_list5_tag(self):
+
+        if ContextListApi.search_platform == "":
+            tag_attribs = all_tag_attrib(ContextListApi.response_root,
+                                         'list/list', '5')
+            value_of_attrib = attrib_value(tag_attribs, 'platform')
+            assert value_of_attrib == "Color"
+
+            value_of_attrib = attrib_value(tag_attribs, 'format')
+            assert value_of_attrib == "json"
+
+            value_of_attrib = attrib_value(tag_attribs, 'link')
+            assert value_of_attrib[-5:] == ".json"
+
+    # Test 'list' 6 tag with any input
+    def test_list6_tag(self):
+
+        if ContextListApi.search_platform == "":
+            tag_attribs = all_tag_attrib(ContextListApi.response_root,
+                                         'list/list', '6')
+            value_of_attrib = attrib_value(tag_attribs, 'platform')
+            assert value_of_attrib == "Windows 10/Threshold"
+
+            value_of_attrib = attrib_value(tag_attribs, 'format')
+            assert value_of_attrib == "json"
+
+            value_of_attrib = attrib_value(tag_attribs, 'link')
+            assert value_of_attrib[-5:] == ".json"
+
+    # Test 'list' 7 tag with any input
+    def test_list7_tag(self):
+
+        if ContextListApi.search_platform == "":
+            tag_attribs = all_tag_attrib(ContextListApi.response_root,
+                                         'list/list', '7')
+            value_of_attrib = attrib_value(tag_attribs, 'platform')
+            assert value_of_attrib == "Office"
+
+            value_of_attrib = attrib_value(tag_attribs, 'format')
+            assert value_of_attrib == "json"
+
+            value_of_attrib = attrib_value(tag_attribs, 'link')
+            assert value_of_attrib[-5:] == ".json"
