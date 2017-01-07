c74ab58b1379baa0a40a8460cf5a486eefd892e8
Kusch-Artem <kuschartem2094@gmail.com>
2017-01-07 18:24:01 +0200

    add categories json tests, debug

---
+++ context/categories_context_json.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from api_logic import random_between_values, request, random_list_value
+
+
+class ContextCategoriesApiJson(object):
+
+    # icon id what will be in request
+
+
+
+    platform = random_list_value(["win8", "ios7", "android", "androidL","color", "win10", "office"])
+
+    print ('''Category Json tests: latform - %s'''
+           % (platform))
+
+    auth_id = '07cb0f621e742888b888d7630c1f0b37bdae536b'
+    payload = {'platform': platform}
+    payload_auth = {'platform': platform, 'auth-id': auth_id}
+
+    # Do Request and return response root
+    response_root = request('categories', payload, "v2", "json")
+    response_root_auth = request('categories', payload_auth, "v2", "json")
+
+    platform_list = ["Windows 8/Metro", "iPhone/iOS 7", "Android", "Android L",
+                     "Color", "Windows 10/Threshold", "Office"]
+
+    platform_code_list = ["win8", "ios7", "android",
+                          "androidL","color", "win10", "office"]
+
+    # Action before class
+    def setup_class(cls):
+        print("\n>>> Class Setup")
+
+    #  Action after class
+    def teardown_class(cls):
+        print("\n>>> Class Teardown")
---
+++ tests/tests_categories_api_json.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from icons8_api.api_logic import json_parse, word_count
+from context.categories_context_json import ContextCategoriesApiJson
+
+
+
+"""
+api_context - file with fixtures and settings
+api_logic - file with main logic of tests
+One class - tested one response with some values
+One test method - test one xml tag and in one tag can be 2+ attributes
+
+python -m pytest -v tests_icon_api.py -s     --   runner
+
+"""
+
+@pytest.fixture(scope="function", params=[
+    (ContextCategoriesApiJson.response_root),
+    (ContextCategoriesApiJson.response_root_auth)
+])
+def param_test(request):
+    return request.param
+
+# Test icon API https://demoapi.icons8.com/api/iconsets/v2/icon
+class TestCategoriesApiJson(ContextCategoriesApiJson):
+
+    # Test parameters object
+    def test_parameters(self, param_test):
+        (json) = param_test
+        assert json_parse(json, ["parameters", "platform"]) == ContextCategoriesApiJson.platform
+        assert json_parse(json, ["parameters", "format"]) == None
+
+    categories_numbers = []
+    categories_count = len(json_parse(ContextCategoriesApiJson.response_root_auth, ["result", "categories"]))
+    for categories in range(categories_count):
+        categories_numbers.append(categories)
+
+    @pytest.mark.parametrize("categories_number", categories_numbers)
+    @pytest.mark.parametrize("json", [ContextCategoriesApiJson.response_root, ContextCategoriesApiJson.response_root_auth])
+    # Test categories object
+    def test_categories(self, categories_number, json):
+        assert len(json_parse(json, ["result", "categories", categories_number, "name"])) >= 2
+        assert len(json_parse(json, ["result", "categories", categories_number, "description"])) >= 0
+
+
+    @pytest.mark.parametrize("categories_number", categories_numbers)
+    @pytest.mark.parametrize("json", [ContextCategoriesApiJson.response_root, ContextCategoriesApiJson.response_root_auth])
+    # Test categories story object
+    def test_categories_story(self, categories_number, json):
+        assert len(json_parse(json, ["result", "categories", categories_number, "story", "text"]).encode('utf-8')) >= 0
+        assert len(json_parse(json, ["result", "categories", categories_number, "story", "author_name"]).encode('utf-8')) >= 0
+        assert len(json_parse(json, ["result", "categories", categories_number, "story", "author_photo"]).encode('utf-8')) >= 0
+        assert len(json_parse(json, ["result", "categories", categories_number, "story", "author_link"]).encode('utf-8')) >= 0
+        assert len(json_parse(json, ["result", "categories", categories_number, "story", "date"]).encode('utf-8')) >= 0
+
+    @pytest.mark.parametrize("categories_number", categories_numbers)
+    @pytest.mark.parametrize("json", [ContextCategoriesApiJson.response_root, ContextCategoriesApiJson.response_root_auth])
+    # Test categories share object
+    def test_categories_share(self, categories_number, json):
+        assert json_parse(json, ["result", "categories", categories_number, "share", "url"])[:20] == "http://demo.ic8.link"
+        assert json_parse(json, ["result", "categories", categories_number, "share", "share_preview"])[:25] == "https://demost.icons8.com"
+        assert json_parse(json, ["result", "categories", categories_number, "share", "icons_preview"])[:25] == "https://demost.icons8.com"
+
+
+
+
+
