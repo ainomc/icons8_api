b6b0d73fd1180540f1dcd5b16b883f8f4ad2412d
Kusch-Artem <kuschartem2094@gmail.com>
2017-01-08 18:39:14 +0200

    add total json api tests

---
+++ context/total_context_json.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from api_logic import random_between_values, request, random_list_value
+
+
+class ContextTotalApiJson(object):
+
+    # icon id what will be in request
+    year = random_list_value(["14", "15", "16"])
+    month =  random_between_values(1, 12)
+    day = random_between_values(1, 31)
+
+    since = '20%s-%s-%s' % (year, month, day)
+    auth_id = '07cb0f621e742888b888d7630c1f0b37bdae536b'
+
+    print ('''Total Json tests: since - %s'''
+           % (since))
+
+    auth_id = '07cb0f621e742888b888d7630c1f0b37bdae536b'
+    payload = {'since': since}
+    payload_auth = {'since': since, 'auth-id': auth_id}
+
+    # Do Request and return response root
+    response_root = request('total', payload, "v3", "json")
+    response_root_auth = request('total', payload_auth, "v3", "json")
+
+    platform_list = ["Windows 8/Metro", "iPhone/iOS 7", "Android", "Android L",
+                     "Color", "Windows 10/Threshold", "Office"]
+
+    platform_code_list = ["win8", "ios7", "android",
+                          "androidL","color", "win10", "office"]
+
+    # Action before class
+    def setup_class(cls):
+        print("\n>>> Class Setup")
+
+    #  Action after class
+    def teardown_class(cls):
+        print("\n>>> Class Teardown")
---
+++ tests/tests_total_api_json.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from icons8_api.api_logic import json_parse
+from context.total_context_json import ContextTotalApiJson
+
+
+
+"""
+api_context - file with fixtures and settings
+api_logic - file with main logic of tests
+One class - tested one response with some values
+One test method - test one xml tag and in one tag can be 2+ attributes
+
+python -m pytest -v tests_icon_api.py -s     --   runner
+
+"""
+
+@pytest.fixture(scope="function", params=[
+    (ContextTotalApiJson.response_root),
+    (ContextTotalApiJson.response_root_auth)
+])
+def param_test(request):
+    return request.param
+
+# Test icon API https://demoapi.icons8.com/api/iconsets/v3/total
+class TestTotaltpiJson(ContextTotalApiJson):
+
+    # Test total object
+    def test_parameters(self, param_test):
+        (json) = param_test
+        assert json_parse(json, ["parameters", "since"]) == ContextTotalApiJson.since
+
+    total_numbers = []
+    total_count = len(json_parse(ContextTotalApiJson.response_root_auth, ["result", "total"]))
+    for total_num in range(total_count):
+        total_numbers.append(total_num)
+
+    @pytest.mark.parametrize("total_numbers", total_numbers)
+    @pytest.mark.parametrize("json", [ContextTotalApiJson.response_root, ContextTotalApiJson.response_root_auth])
+    # Test total object
+    def test_total(self, total_numbers, json):
+        assert ContextTotalApiJson.platform_list.count(
+            json_parse(json, ["result", "total", total_numbers, "name"])) == 1
+        assert ContextTotalApiJson.platform_code_list.count(
+            json_parse(json, ["result", "total", total_numbers, "api_code"])) == 1
+        assert json_parse(json, ["result", "total", total_numbers, "total"]) >= 0
