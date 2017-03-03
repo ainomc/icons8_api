c24a80a35855f6eebac2291d825b8a72bbdcef2a
Kusch-Artem <kuschartem2094@gmail.com>
2017-03-03 17:14:25 +0200

    try fix junit command

--- init.py
+++ init.py
@@ -3,6 +3,7 @@ import json
 import os
 import glob
 import argparse
+from sys import platform
 
 #runner demo - python init.py -request_url demoapi -auth_id 07cb0f621e742888b888d7630c1f0b37bdae536b
 
@@ -25,7 +26,11 @@ for file_num in range(len(list_test_fies)):
     list_test_fies[file_num] = os.path.join(os.getcwd(), 'tests', list_test_fies[file_num])
 
 str_list = " ".join(str(x) for x in list_test_fies) # convert list to string
-os.system(r'python -m pytest -v %s -s --showlocals --junitxml=junitxml' % str_list)
+
+if "win" in platform:
+    os.system(r'python -m pytest -v %s -s --showlocals' % str_list)
+elif "linux" in platform:
+    os.system(r'python -m pytest -v %s -s --showlocals --junitxml=/var/lib/jenkins/workspace/icons8api_tests/junitxml' % str_list)
 
 
 
